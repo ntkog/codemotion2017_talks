f27fa99d96e4dba6c36c02939ff392f88afceced
Jorge Barrachina <nusquema@gmail.com>
2017-08-18 14:03:18 +0200

    modified createCorpusesfromArr supporting now arrays of descriptions

--- examples/stats.js
+++ examples/stats.js
@@ -6,7 +6,14 @@ const TOPICS = require('../data/topics.json');
 
 var log = require('debug')('summary')
 
+let allTalks = Listing.allTalks({
+  descFilterCb : [tools.filters.cleanDescription]
+});
+
+/* Uncomment this if you want to play with your own descFilters
+
 let allTalks = Listing.allTalks();
+*/
 
 TOPICS.map(function (topic) {
    ["women","men","mix"].map(function (gender) {
@@ -25,3 +32,27 @@ TOPICS.map(function (topic) {
      outputFolder : `${process.env["HOME"]}/Desktop/stats_all`
    });
 });
+
+// Any topic any gender
+nlp.summary(allTalks,{
+  ocurrencesThreshold : 5,
+  toJson : true,
+  outputFolder : `${process.env["HOME"]}/Desktop/stats_all`,
+  filename : 'all_talks.json'
+});
+
+// all authors Description
+
+let authorsDescription = Listing.allTalks({ fieldNames: ["authors","language","title"] })
+   .map((t) => ({
+     description: t.authors.map((author) => author.description).filter((desc) => desc),
+     language : t.language,
+     title: t.title
+   }));
+
+nlp.summary(authorsDescription, {
+  ocurrencesThreshold : 5,
+  toJson : true,
+  outputFolder : `${process.env["HOME"]}/Desktop/stats_all`,
+  filename : 'all_authors_description.json'
+})
